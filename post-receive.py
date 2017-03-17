2b550e2ade074f7811902473803681a6a65800db
Hrvoje Novosel <hnovosel@live.com>
2017-03-17 15:25:31 +0100

    + project name parse

--- post-receive.py
+++ post-receive.py
@@ -8,11 +8,12 @@
 
 """
 
-import os
 import sys
+import os
 import json
+import subprocess
 
-
+DEBUG = True
 # path to config file contaning information on target email addresses, servers, ...
 CONFIG_FILE = './../config.json'
 
@@ -22,6 +23,20 @@ def Log(message):
 		Message logging function
 	"""
 	print("Push notification: {}".format(message))
+
+
+def run_command(command, panic=False, nlines=None):
+	"""
+		Runs command and returns last output line
+		If panic return value of the command must be 0
+	"""
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	retval = p.stdout.readlines()
+	if p.wait() != 0 and panic:
+		Log('Cannot get extract repository info, exiting ...')
+		return ''
+
+	return retval.pop()
 
 
 def get_configuration():
@@ -36,22 +51,45 @@ def get_configuration():
 
 	return None
 
+def repo_details(git_path, property):
+	"""
+		Reads git description fp and returns basic info on git repository
+	"""
+	fname = git_path + '/'
+	if property == 'project':
+		fname += 'description'
+
+	if os.path.isfile(fname):
+		with open(fname, 'r') as fp:
+			return fp.read()
+	else:
+		return 'undefined'
+
 
 def main():
 	"""
 		Perform the stuff
 	"""
+	# check for basic git info first
+	git_dir = run_command('git rev-parse --git-dir', True).strip()
+	git_project = repo_details(git_dir, 'project')
+
+	print git_project
+
 	conf = get_configuration()
 	if conf is None:
 		Log('Valid configuration missing')
 		return
 
-	print(conf)
+	# print(conf)
 	# check for modified files
 
 	# msg and file diff, see what to do with that
 
 
 if __name__ == '__main__':
+	"""
+		TODO: utilize debug/prod env
+	"""
 	print sys.argv
 	main()
