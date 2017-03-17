d4ca303c37112d0a1bd42922f3a00732eadd14c6
Hrvoje Novosel <hnovosel@live.com>
2017-03-17 14:06:42 +0100

    * init commit

---
+++ post-receive.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python2.7
+"""
+
+	Simple python script for push notifications. Notifications ...
+
+	Author: Hrvoje Novosel<hrvojedotnovosel@gmail.com>
+	Created: 17. Mar 2017
+
+"""
+
+
+def main():
+	"""
+		Perform the stuff
+	"""
+	print 'let start coding'
+
+
+
+if __name__ == '__main__':
+	main()
