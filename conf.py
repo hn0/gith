c649078b75d82bd55859124cfee798092ba31e38
Hrvoje Novosel <hnovosel@live.com>
2017-03-20 08:47:53 +0100

    * code refractoring

--- conf.py
+++ conf.py
@@ -3,11 +3,14 @@
 
 """
 
+DEBUG = True
 # path to config file contaning information on target email addresses, servers, ...
 CONFIG_FILE = './../config.json'
 
 import os
+import sys
 import json
+import subprocess
 
 def get_configuration():
 	"""
@@ -20,3 +23,63 @@ def get_configuration():
 		Log('Configuration file not present on the system')
 
 	return None
+
+
+def read_commits():
+	"""
+		Generator function of commits from std in
+	"""
+	for ln in sys.stdin:
+		args = ln.split(' ')
+		if len(args) == 3:
+			yield args
+		else:
+			yield None
+
+
+# a set of helper functions
+def Log(message):
+	"""
+		Message logging function
+	"""
+	print("Push notification: {}".format(message))
+
+
+def run_command(command, panic=False, nlines=1):
+	"""
+		Runs command and returns last output line
+		If panic return value of the command must be 0
+	"""
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	retval = p.stdout.readlines()
+	if p.wait() != 0 and panic:
+		Log('Cannot get extract repository info, exiting ...')
+		sys.extit()
+
+	if nlines == 1:
+		return retval.pop()
+	else:
+		if nlines == 'max':
+			nln = len(retval)
+		else:
+			nln = min(len(retval), nlines)
+		retstr = ""
+		for i in range(nln):
+			retstr += retval[i]
+
+		return retstr
+
+
+def repo_details(git_path, property):
+	"""
+		Reads git description fp and returns basic info on git repository
+	"""
+	fname = git_path + '/'
+	if property == 'project':
+		fname += 'description'
+
+	if os.path.isfile(fname):
+		with open(fname, 'r') as fp:
+			return fp.read()
+	else:
+		return 'undefined'
--- post-receive.py
+++ post-receive.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python2.7
 """
 
-	Simple python script for push notifications. Notifications ...
+	Simple hook for sending server push summary notification message
 
 	Line for stdin (test example):
 	fc88f2f47129347ee20f73632927e84dae67c4f7 1de59c8239ea271a9cb52e8b6a7b50d3939207b3 refs/heads/master
@@ -15,60 +15,7 @@
 
 """
 
-import sys
-import os
-import json
-import subprocess
 import conf
-
-DEBUG = True
-
-def Log(message):
-	"""
-		Message logging function
-	"""
-	print("Push notification: {}".format(message))
-
-
-def run_command(command, panic=False, nlines=1):
-	"""
-		Runs command and returns last output line
-		If panic return value of the command must be 0
-	"""
-	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-	retval = p.stdout.readlines()
-	if p.wait() != 0 and panic:
-		Log('Cannot get extract repository info, exiting ...')
-		sys.extit()
-
-	if nlines == 1:
-		return retval.pop()
-	else:
-		if nlines == 'max':
-			nln = len(retval)
-		else:
-			nln = min(len(retval), nlines)
-		retstr = ""
-		for i in range(nln):
-			retstr += retval[i]
-
-		return retstr
-
-
-def repo_details(git_path, property):
-	"""
-		Reads git description fp and returns basic info on git repository
-	"""
-	fname = git_path + '/'
-	if property == 'project':
-		fname += 'description'
-
-	if os.path.isfile(fname):
-		with open(fname, 'r') as fp:
-			return fp.read()
-	else:
-		return 'undefined'
-
 
 def send_message(recipient, title, message, server):
 	"""
@@ -83,9 +30,10 @@ def main():
 	"""
 		Perform the stuff
 	"""
+	Log = conf.Log
 	# check for basic git info first
-	git_dir = run_command('git rev-parse --git-dir', True).strip()
-	git_project = 'Project ' + repo_details(git_dir, 'project')
+	git_dir = conf.run_command('git rev-parse --git-dir', True).strip()
+	git_project = 'Project ' + conf.repo_details(git_dir, 'project')
 
 	cfg = conf.get_configuration()
 	if cfg is None:
@@ -94,13 +42,12 @@ def main():
 
 	msg = ''
 	# check for modified files
-	for ln in sys.stdin:
-		args = ln.split(' ')
-		if len(args) == 3:
+	for args in conf.read_commits():
+		if not args is None:
 			git_project += ' branch: ' + args[2][args[2].rfind('/')+1:]
-			commit = run_command(' git log -n1 --format="%an<%ae> %s" ' + args[1] + ' ' + args[2])
-			diff   = run_command('git diff --stat {} {} {}'.format(args[1], args[0], args[2]), False, 'max')
-
+			commit = conf.run_command(' git log -n1 --format="%an<%ae> %s" ' + args[1] + ' ' + args[2])
+			diff   = conf.run_command('git diff --stat {} {} {}'.format(args[1], args[0], args[2]), False, 'max')
+	
 			# TODO: see if summary of changes will be needed?
 			msg += "Commit:\n{}\nDiff:\n{}\n\n".format(commit, diff)
 
@@ -112,6 +59,6 @@ def main():
 
 if __name__ == '__main__':
 	"""
-		TODO: utilize debug/prod env
+		Scripts entry point
 	"""
 	main()
